5f2ae37468df580bd3c2124fb1feeed9da7c9ec5
Joel Binny <joelbinny2003@gmail.com>
2024-11-27 15:06:39 +0400

    first commit

---
+++ activities.js
@@ -0,0 +1,131 @@
+const activities = [
+    {
+        id: 1,
+        title: "Art Class",
+        description: "Learn to paint and create beautiful art.",
+        price: 100,
+        image: "art.jpg",
+        availableInventory: 5,
+        rating: 4,
+        location: "Downtown dubai"
+    },
+    {
+        id: 2,
+        title: "Dance Class",
+        description: "Join our fun dance classes for all ages.",
+        price: 120,
+        image: "dance.jpg",
+        availableInventory: 5,
+        rating: 5,
+        location: "Deira"
+    },
+    {
+        id: 3,
+        title: "Coding class",
+        description: "interactive way for kids to learn coding basics and build simple robots.",
+        price: 150,
+        image: "coding.jpeg",
+        availableInventory: 5,
+        rating: 3,
+        location: "Sharjah"
+    },
+    
+    {
+        id: 4,
+        title: "Outdoor Adventure Club",
+        description: "explore nature through hiking and outdoor games, building confidence and teamwork.",
+        price: 150,
+        image: "outdoor.jpg",
+        availableInventory: 5,
+        rating: 3,
+        location: "Abu dhabi"
+    },
+
+    {
+        id: 5,
+        title: "Drama Club",
+        description: "A fun space for kids to act, create characters, and produce plays.",
+        price: 150,
+        image: "drama.jpg",
+        availableInventory: 5,
+        rating: 3,
+        location: "Ras Al Khaimah"
+    },
+    {
+        id: 6,
+        title: "Cooking lessons",
+        description: "Kids learn basic cooking skills, try new recipes, and gain confidence in the kitchen.",
+        price: 150,
+        image: "cooking.jpg",
+        availableInventory: 5,
+        rating: 3,
+        location: "JVC"
+    },
+
+    {
+        id: 7,
+        title: "Chess Club",
+        description: "strategic and engaging activity where kids learn to think ahead.",
+        price: 150,
+        image: "chess.jpeg",
+        availableInventory: 6,
+        rating: 3,
+        location: "DSF"
+    },
+
+    {
+        id: 8,
+        title: "Martial Arts Lessons",
+        description: "build discipline, fitness, and self-respect while learning basic martial arts .",
+        price: 60,
+        image: "marts.jpeg",
+        availableInventory: 5,
+        rating: 3,
+        location: "Marina"
+        
+    },
+
+    {
+        id: 9,
+        title: "Gardening Club",
+        description: "Encourages kids to connect with nature, learn about plants, and practice responsibility .",
+        price: 10,
+        image: "garden.jpeg",
+        availableInventory: 5,
+        rating: 3,
+        location: "Warqa"
+    },
+
+    {
+        id: 10,
+        title: "Language and Culture Club",
+        description: "a fun introduction to a new language and culture.",
+        price: 50,
+        image: "culture.jpeg",
+        availableInventory: 5,
+        rating: 3,
+        location: "Dubai Hills"
+    },
+
+    {
+        id: 11,
+        title: " Book Club",
+        description: "kids read and discuss stories together, boosting reading comprehension.",
+        price: 120,
+        image: "book.jpg",
+        availableInventory: 5,
+        rating: 3,
+        location: "Bur Dubai"
+    },
+
+    {
+        id: 12,
+        title: "Sports Lessons",
+        description: "An active club where kids try different sports, improve physical fitness, and learn teamwork.",
+        price: 130,
+        image: "sports.jpg",
+        availableInventory: 5,
+        rating: 3,
+        location: " Barsha"
+    }
+];
